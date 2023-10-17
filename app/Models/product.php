89481ceebbacdebd7c9de14306df03c8977f9ccc
vincent van de merwe <335479@student.mboutrecht.nl>
2023-10-17 17:00:22 +0200

    1 table with query's and 1 table with eloquent

---
+++ app/Models/product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'shop_id',
+        'product_name',
+        'artikel_nr'
+    ];
+}
